abd04cad951032f179a8a3f50f102189f3cb76e6
andrushchenko <andrushchenko@yandex-team.ru>
2024-01-27 15:20:18 +0300

    Remove comments from locals.tf file

--- deployments/terraform/locals.tf
+++ deployments/terraform/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  zone_a_v4_cidr_blocks = "10.1.0.0/16" # Set the CIDR block for subnet in the ru-central1-a availability zone.
-  folder_id             = "b1grti8d755eddfv0jcp"            # Set your cloud folder ID.
-  k8s_version           = "1.27"            # Set the Kubernetes version.
-  sa_name               = "cluster-swiply"            # Set a service account name. It must be unique in a cloud.
+  zone_a_v4_cidr_blocks = "10.1.0.0/16"
+  folder_id             = "b1grti8d755eddfv0jcp"
+  k8s_version           = "1.27"
+  sa_name               = "cluster-swiply"
 }
