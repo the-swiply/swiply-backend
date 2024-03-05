2fd56a35e90554ee8a758a08d7f007c1fe734fe9
Dmitriy Shagarov <dshagarov@ozon.ru>
2024-03-05 22:24:52 +0300

    add data collect

---
+++ recommendation/migrations/20240305191828_create_interaction.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS interaction
+(
+    id         uuid,
+    "from"     uuid,
+    "to"       uuid,
+    positive   bool,
+    updated_at timestamp
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS interaction;
+-- +goose StatementEnd
