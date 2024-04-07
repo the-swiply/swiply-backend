8cdb3db8390b3740ece680b90860142f0c93eaad
Dmitriy Shagarov <dshagarov@ozon.ru>
2024-04-07 19:47:07 +0300

    update deps

--- oracle/app/service/oracle.py
+++ oracle/app/service/oracle.py
@@ -1,5 +1,12 @@
 from app.api.oracle import oracle_pb2_grpc, oracle_pb2
 from app.houston import loggy
+import pandas as pd
+import lightfm
+import numpy as np
+from lightfm.data import Dataset
+from lightfm.cross_validation import random_train_test_split
+from lightfm import LightFM
+from lightfm.evaluation import precision_at_k
 
 
 class OracleService(oracle_pb2_grpc.OracleServicer):
